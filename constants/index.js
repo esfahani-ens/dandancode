d951439c020bfc33437ecbc2a97adbd717d5f6b7
esfahani-ens <107998444+esfahani-ens@users.noreply.github.com>
2023-07-20 02:56:57 +0300

    Update index.js
    Commit Change

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x263d92bf37ab83094a8c695da016d2145635e1cf";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
